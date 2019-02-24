0501b5375077974c2365b6701be90f7f97959bb7
aman <aman_mehta@hotmail.com>
2019-02-24 15:20:33 +1100

    initial commit

---
+++ src/app/bills/bills.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bills',
+  templateUrl: 'bills.page.html',
+  styleUrls: ['bills.page.scss']
+})
+export class BillsPage {}
