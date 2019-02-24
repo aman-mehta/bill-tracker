f15f5965d3c0d6509de801da131324b27b278ad8
aman-mehta <aman_mehta@hotmail.com>
2019-02-24 17:36:39 +1100

    added ability to add bills

--- src/app/bills/bills.page.ts
+++ src/app/bills/bills.page.ts
@@ -1,8 +1,86 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-bills',
   templateUrl: 'bills.page.html',
   styleUrls: ['bills.page.scss']
 })
-export class BillsPage {}
+export class BillsPage {
+
+  bills: Object[] = [];
+
+  constructor(
+    public alertController: AlertController,
+    private toastController: ToastController
+  ) {
+    this.bills = [
+      {
+        name: 'rent',
+        amount: 300,
+        rate: 'weekly'
+      },
+      {
+        name: 'phone',
+        amount: 40,
+        rate: 'monthly'
+      },
+      {
+        name: 'internet',
+        amount: 60,
+        rate: 'monthly'
+      },
+      {
+        name: 'electricity',
+        amount: 300,
+        rate: 'quarterly'
+      }
+    ];
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'New Bill',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'name'
+        },
+        {
+          name: 'amount',
+          type: 'number',
+          placeholder: 'amount'
+        },
+        {
+          name: 'rate',
+          type: 'text',
+          placeholder: 'weekly'
+        }
+      ],
+      buttons: [
+        {
+          text: 'OK',
+          handler: (data) => {
+            this.bills.push(data);
+            this.presentToast(data);
+          }
+        }, 
+        'Cancel']
+    });
+
+    await alert.present();
+  }
+
+  async presentToast(data) {
+    const toast = await this.toastController.create({
+      message: data.name + ' bill added!',
+      color: 'dark',
+      showCloseButton: true,
+      position: 'bottom',
+      closeButtonText: 'done',
+      duration: 2000
+    });
+
+    await toast.present();
+  }
+}
